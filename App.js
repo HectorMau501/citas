3c4c4f8b68022e6bcbf372b9c0d62ddb603461ee
HectorMau501 <mauhector7@gmail.com>
2023-11-21 19:31:15 -0600

    Llevo hasta editar citas

--- App.js
+++ App.js
@@ -1,104 +1,139 @@
 //Componentes de React
-import React, { useState } from 'react'; //Hooks es el de useState. Regla 1 de hooks se coloca en la parte principal de los componentes
+import React, {useState} from 'react' //Hooks es el de useState. Regla 1 de hooks se coloca en la parte principal de los componentes
 import {
   SafeAreaView,
   View, //View se comporta como un div en html y es necesario porque no vas a poder retornar dos text
   Text,
   StyleSheet, //Es como el css pero en React
-  Button, 
+  Button,
   Pressable, //Este tienes mas tipos de eventos que button, como dejar presionado, presionado y soltar y asi
-  Modal //Es para desplegar una ventana emergente.
+  Modal, //Es para desplegar una ventana emergente.
+  FlatList,
 } from 'react-native'
-import Formulario from './src/Formulario';
+import Formulario from './src/Formulario'
+import Paciente from './src/Paciente'
 
 //Siempre vamos a tener un return en nuestros componentes sino nos va a marca error
 const App = () => {
-
   //Aqui se  colocan los hooks
-  const [modalVisible, setModalVisible ] = useState(false) //Asi se declaran los hooks, otra regla de los hooks es que no se puede regitrar si esta en una condicion
+  const [modalVisible, setModalVisible] = useState(false) //Asi se declaran los hooks, otra regla de los hooks es que no se puede regitrar si esta en una condicion
   const [pacientes, setPacientes] = useState([])
-
+  const [paciente, setPaciente] = useState({})
 
   // Antes del return aqui se pone todo tipo de codigo de javaScript
 
+  const pacienteEditar = id => {
+    const pacienteEditar = pacientes.filter(paciente => paciente.id === id)
+
+    setPaciente(pacienteEditar[0])
+  }
 
   return (
     //  El componentes SafeArea sirve para bajar todo el texto y componentes en ios para el noch
-    // <SafeAreaView> 
+    // <SafeAreaView>
     // </SafeAreaView>
     <SafeAreaView style={styles.container}>
       {/* Se puede anidar componenets */}
-      <Text style={styles.titulo}>Administrador de 
-      Citas {''}
-      <Text style={styles.tituloBold}>Veterinarias</Text>
+      <Text style={styles.titulo}>
+        Administrador de Citas {''}
+        <Text style={styles.tituloBold}>Veterinarias</Text>
       </Text>
-      <Pressable
-      style={styles.btnNuevaCita}
-      >
+      <Pressable style={styles.btnNuevaCita}>
         <Text
-        style={styles.btnTextoNuevaCita}
-        onPress={() => setModalVisible(!modalVisible)}
-        >
-        Nueva Cita</Text>
+          style={styles.btnTextoNuevaCita}
+          onPress={() => setModalVisible(!modalVisible)}>
+          Nueva Cita
+        </Text>
       </Pressable>
 
       <Modal
-      animationType='slide' 
-      visible= {modalVisible} //animation y visible es para cambiar al modal
+        animationType="slide"
+        visible={modalVisible} //animation y visible es para cambiar al modal
       >
         {/* <Text>Desde Modal</Text> */}
       </Modal>
 
-      <Formulario 
+      {pacientes.length === 0 ? 
+        <Text style={styles.noPacientes}>No hay pacientes aun</Text>
+       : 
+        <FlatList //FlatList es para mostrar las variables
+        style={styles.listado}
+          data={pacientes} //data va hacer referecia a los datos que va a redendizar
+          keyExtractor={item => item.id} //Va a buscar en el arreglo de pacientes o en los datos que le diste en el flatlist o bien iterar sobre los elementos
+          renderItem={({item}) => {
+            //El componentes que se van a mostrar cuando comience a irerar sobre los elementos
+            return(
+              <Paciente 
+              item={item}
+              setModalVisible={setModalVisible}
+              pacienteEditar={pacienteEditar}
+              />
+            ) 
+          }}
+        />
+      }
+
+      <Formulario
         // prob se llama modalVisible igual que la variable
         // los prob se pasan del hijo al padre
         // sirven para que los state puedan estar disponibles en otros componentes hijos
-        modalVisible = {modalVisible} //Este es un prob, estoy pasando la variable que es nuestro state
+        modalVisible={modalVisible} //Este es un prob, estoy pasando la variable que es nuestro state
         pacientes={pacientes}
         setModalVisible={setModalVisible}
         setPacientes={setPacientes}
+        paciente={paciente}
       />
 
       {/* <Button  */}
       {/* title='Nueva Cita' */}
       {/* onPress={() => { */}
-        {/* console.log('Presionaste en el boton') */}
+      {/* console.log('Presionaste en el boton') */}
       {/* }} //Para asociar un evento con los botones, andrentro del podras tener javaScript */}
       {/* ></Button> */}
     </SafeAreaView>
-  );
-};
+  )
+}
 
 //Por buenas practicas asi se declara la variable
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: '#F3F4F6',
-    flex: 1 //Esto sirve para que todo el contenido este en la parte de arriba hacia abajo
+    flex: 1, //Esto sirve para que todo el contenido este en la parte de arriba hacia abajo
   },
   titulo: {
     textAlign: 'center',
     textTransform: 'uppercase',
     fontSize: 20,
-    color: "#374151",
-    fontWeight: 'bold'
-  }, 
-  tituloBold:{
+    color: '#374151',
+    fontWeight: 'bold',
+  },
+  tituloBold: {
     fontWeight: '900',
     color: '#6D28D9',
   },
-  btnNuevaCita:{
+  btnNuevaCita: {
     backgroundColor: '#6D28D9',
     padding: 15,
     marginTop: 30,
     marginHorizontal: 20, //Este marginHorizontal es como si tuvieras el mismo margin para left y right
-    borderRadius: 10
+    borderRadius: 10,
   },
-  btnTextoNuevaCita:{
+  btnTextoNuevaCita: {
     textAlign: 'center',
     color: '#FFF',
     fontWeight: '900',
-    textTransform: 'uppercase'
-  }
+    textTransform: 'uppercase',
+  },
+  noPacientes: {
+    marginTop: 40,
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  listado: {
+    marginTop: 50,
+    marginHorizontal: 30
+  },
 })
 
-export default App;
+export default App
--- src/Formulario.js
+++ src/Formulario.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { 
+import React, {useState, useEffect} from 'react';
+import {
   View,
   Modal,
   Text,
@@ -8,215 +8,240 @@ import {
   SafeAreaView,
   ScrollView,
   Pressable,
-  Alert
-} from 'react-native'
-import DatePicker from 'react-native-date-picker'
+  Alert,
+} from 'react-native';
+import DatePicker from 'react-native-date-picker';
 
-const Formulario = ({modalVisible, setModalVisible, pacientes, setPacientes}) => {
+const Formulario = ({
+  modalVisible,
+  setModalVisible,
+  pacientes,
+  setPacientes,
+  paciente: pacienteObj
+}) => {
+  const [paciente, setPaciente] = useState('');
+  const [id, setId] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [fecha, setFecha] = useState(new Date());
+  const [sintomas, setSintomas] = useState('');
 
-  const [paciente, setPaciente] = useState('')
-  const [propietario, setPropietario] = useState('')
-  const [email, setEmail] = useState('')
-  const [telefono, setTelefono] = useState('')
-  const [fecha, setFecha] = useState(new Date())
-  const [sintomas, setSintomas] = useState('')
+  useEffect(() => {
+    if(Object.keys(pacienteObj).length > 0){
+      setId(pacienteObj.id)
+      setPaciente(pacienteObj.paciente)
+      setPropietario(pacienteObj.propietario)
+      setEmail(pacienteObj.email)
+      setTelefono(pacienteObj.telefono)
+      setFecha(pacienteObj.fecha)
+      setSintomas(pacienteObj.sintomas)
+    }
+  }, [pacienteObj])
+
+  //Hook useEffect si declaramos un state podemos tener un useEffect que este revisando cuando aya cambios en este state
+  //Sirve ejecutarse automaticamente cuando el componentes esta listo,  colocar codigo para consultar una API o LocalStorage
 
   const handleCita = () => {
     //Validar que haya contenido en los input
-    if([paciente, propietario, email, fecha, sintomas].includes('')){
-
+    if ([paciente, propietario, email, fecha, sintomas].includes('')) {
       //El alert toma tres argumentos uno va hacer el encabezado, cuerpo y el texto del boton
       Alert.alert(
         'Error',
         'Todos los campos son Obligatorios',
         // [{text:'OK'}, {text:'Cancelar'}, {text:'Neutral'}]//Aqui es el boton, puedes agregar mas botones
-      )
-        return //Es importante el return ya que sino va a continuar con el proceso y va a agregar
+      );
+      return; //Es importante el return ya que sino va a continuar con el proceso y va a agregar
     }
 
     const nuevoPaciente = {
+      id: Date.now(), //Sirve para retornar valores diferentes
       paciente,
       propietario,
       email,
       telefono,
       fecha,
-      sintomas
-    }
-    setPacientes([...pacientes, nuevoPaciente])
+      sintomas,
+    };
+    setPacientes([...pacientes, nuevoPaciente]);
+    setModalVisible(!modalVisible);
 
-  }
+    setPaciente(''); //Para recetear
+    setPropietario('');
+    setEmail('');
+    setTelefono('');
+    setFecha(new Date());
+    setSintomas('');
+  };
 
   return (
-      <Modal
-        animationType='slide' 
-        visible= {modalVisible} //animation y visible es para cambiar al modal
-      >
-        <SafeAreaView style={styles.contenido}>
-          <ScrollView>      
-            <Text style={styles.titulo}>Nueva {''}
-              <Text style={styles.tituloBold}>Cita</Text>
-            </Text>
+    <Modal
+      animationType="slide"
+      visible={modalVisible} //animation y visible es para cambiar al modal
+    >
+      <SafeAreaView style={styles.contenido}>
+        <ScrollView>
+          <Text style={styles.titulo}>
+            Nueva {''}
+            <Text style={styles.tituloBold}>Cita</Text>
+          </Text>
 
-            <Pressable 
-              style={styles.btnCancelar}
-              onLongPress={() => setModalVisible(!modalVisible)}
-            >
-              <Text style={styles.btnCancelarTexto}>X Cancelar</Text>
-            </Pressable>
-
-          <View style = {styles.campo}>
-            <Text style = {styles.label}>Nombre Paciente</Text>
-            <TextInput
-                style = {styles.input}
-                placeholder = 'Nombre Paciente'//Recuerda este es para text en el input
-                placeholderTextColor = {'#666'}
-                value={paciente} //Esto es para la variable de paciente
-                onChangeText={setPaciente} //Para que reciba el cambio
-            />
-          </View>
-
-          <View style = {styles.campo}>
-            <Text style = {styles.label}>Nombre Propietario</Text>
-            <TextInput
-                style = {styles.input}
-                placeholder = 'Nombre Propietario'//Recuerda este es para text en el input
-                placeholderTextColor = {'#666'}
-                value={propietario} 
-                onChangeText={setPropietario}
-            />
-          </View>
-
-          <View style = {styles.campo}>
-            <Text style = {styles.label}>Email Propietario</Text>
-            <TextInput
-                style = {styles.input}
-                placeholder = 'Email Propietario'//Recuerda este es para text en el input
-                placeholderTextColor = {'#666'}
-                keyboardType = 'email-address'
-                value = {email}
-                onChangeText={setEmail}
-            />
-          </View>
-
-          <View style = {styles.campo}>
-            <Text style = {styles.label}>Teléfono Propietario</Text>
-            <TextInput
-                style = {styles.input}
-                placeholder = 'Teléfono Propietario'//Recuerda este es para text en el input
-                placeholderTextColor = {'#666'}
-                keyboardType = 'number-pad'
-                value={telefono}
-                onChangeText={setTelefono}
-                maxLength={10}//Para el maximo de letras o numeros
-            />
-          </View>
-
-          <View style = {styles.campo}>
-            <Text style = {styles.label}>Fecha Alta</Text>
-              <View style={styles.fechaContenedor}>
-                <DatePicker
-                  date={fecha}
-                  locale='es'
-                  onDateChange={ (date) => setFecha(date) }
-                />
-              </View>
-          </View>
-
-          <View style = {styles.campo}>
-            <Text style = {styles.label}>Síntomas</Text>
-            <TextInput
-                style = {[styles.input, styles.sintomasInput]}
-                placeholder = 'Síntomas Paciente'//Recuerda este es para text en el input
-                placeholderTextColor = {'#666'}
-                value={sintomas}
-                onChangeText={setSintomas}
-                //Estas dos ultimas propiedades son para poder poner un input pas grande 
-                //y que haya mas lineas
-                multiline={true}
-                numberOfLines={4}
-            />
-          </View>
-
-          <Pressable 
-            style={styles.btnNuevaCita}
-            onPress={handleCita}
-          >
-              <Text style={styles.btnNuevaCitaTexto}>Agregar Paciente</Text>
+          <Pressable
+            style={styles.btnCancelar}
+            onLongPress={() => setModalVisible(!modalVisible)}>
+            <Text style={styles.btnCancelarTexto}>X Cancelar</Text>
           </Pressable>
 
-          </ScrollView>
-        </SafeAreaView>
-      </Modal>
-  )
-}
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Paciente</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Paciente" //Recuerda este es para text en el input
+              placeholderTextColor={'#666'}
+              value={paciente} //Esto es para la variable de paciente
+              onChangeText={setPaciente} //Para que reciba el cambio
+            />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Nombre Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre Propietario" //Recuerda este es para text en el input
+              placeholderTextColor={'#666'}
+              value={propietario}
+              onChangeText={setPropietario}
+            />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Email Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Email Propietario" //Recuerda este es para text en el input
+              placeholderTextColor={'#666'}
+              keyboardType="email-address"
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Teléfono Propietario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Teléfono Propietario" //Recuerda este es para text en el input
+              placeholderTextColor={'#666'}
+              keyboardType="number-pad"
+              value={telefono}
+              onChangeText={setTelefono}
+              maxLength={10} //Para el maximo de letras o numeros
+            />
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Fecha Alta</Text>
+            <View style={styles.fechaContenedor}>
+              <DatePicker
+                date={fecha}
+                mode="date"
+                locale="es"
+                onDateChange={date => setFecha(date)}
+              />
+            </View>
+          </View>
+
+          <View style={styles.campo}>
+            <Text style={styles.label}>Síntomas</Text>
+            <TextInput
+              style={[styles.input, styles.sintomasInput]}
+              placeholder="Síntomas Paciente" //Recuerda este es para text en el input
+              placeholderTextColor={'#666'}
+              value={sintomas}
+              onChangeText={setSintomas}
+              //Estas dos ultimas propiedades son para poder poner un input pas grande
+              //y que haya mas lineas
+              multiline={true}
+              numberOfLines={4}
+            />
+          </View>
+
+          <Pressable style={styles.btnNuevaCita} onPress={handleCita}>
+            <Text style={styles.btnNuevaCitaTexto}>Agregar Paciente</Text>
+          </Pressable>
+        </ScrollView>
+      </SafeAreaView>
+    </Modal>
+  );
+};
 
 const styles = StyleSheet.create({
-  contenido:{
+  contenido: {
     backgroundColor: '#6D28D9',
-    flex: 1
+    flex: 1,
   },
   titulo: {
     fontSize: 30,
     fontWeigh: '600',
     textAlign: 'center',
     marginTop: 30,
-    color: '#FFF'
+    color: '#FFF',
   },
-  tituloBold:{
-    fontWeight: '900'
+  tituloBold: {
+    fontWeight: '900',
   },
-  btnCancelar:{
-     marginVertical: 30,
-     backgroundColor: '#5827A4',
-     marginHorizontal: 30,
-     padding: 15  ,
-     borderRadius: 10,
-     textTransform: 'uppercase',
+  btnCancelar: {
+    marginVertical: 30,
+    backgroundColor: '#5827A4',
+    marginHorizontal: 30,
+    padding: 15,
+    borderRadius: 10,
+    textTransform: 'uppercase',
   },
-  btnCancelarTexto:{
+  btnCancelarTexto: {
     color: '#FFF',
     textAlign: 'center',
     fontWeight: '900',
     fontSize: 16,
   },
-  campo:{
+  campo: {
     marginTop: 10,
     marginHorizontal: 30,
   },
   label: {
-      color: '#FFF',
-      marginBottom: 10,
-      margintop: 15,
-      fontSize: 20,
-      fontWeight: '600'
+    color: '#FFF',
+    marginBottom: 10,
+    margintop: 15,
+    fontSize: 20,
+    fontWeight: '600',
   },
   input: {
     backgroundColor: '#FFF',
     padding: 15,
     borderRadius: 10,
   },
-  sintomasInput:{
-    height: 100
+  sintomasInput: {
+    height: 100,
   },
-  fechaContenedor:{
+  fechaContenedor: {
     backgroundColor: '#FFF',
     borderRadius: 10,
     padding: 5,
   },
-  btnNuevaCita:{
+  btnNuevaCita: {
     marginVertical: 50,
     backgroundColor: '#F59E0B',
     paddingVertical: 10,
     marginHorizontal: 30,
-    borderRadius: 10
+    borderRadius: 10,
   },
-  btnNuevaCitaTexto:{
+  btnNuevaCitaTexto: {
     textAlign: 'center',
     color: '#5827A4',
     textTransform: 'uppercase',
     fontWeight: '900',
-    fontSize: 16
-  }
-})
+    fontSize: 16,
+  },
+});
 
-export default Formulario
+export default Formulario;
